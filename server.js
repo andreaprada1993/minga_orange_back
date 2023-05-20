9eb5879fcec1bd98f796674504c2ad8f97f76352
andreaprada1993 <andrea_prada_93@hotmail.com>
2023-05-19 22:43:31 -0500

    se realizo la coneccion a la base de datos

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import "dotenv/config.js";
+import "./config/database.js";
 
 const server = express();
 
 const PORT = process.env.PORT || 8080;
 
 const ready = () => console.log("server ready: " + PORT);
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 server.listen(PORT, ready); 

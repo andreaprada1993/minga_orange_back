4da8439557ed4fd830f5dcb2b3424699261b5646
andreaprada1993 <andrea_prada_93@hotmail.com>
2023-05-26 20:07:47 -0500

    se hizo router

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from "express";
 import "dotenv/config.js";
 import "./config/database.js";
+import indexRouter from "./router/index.js";
+import cors from 'cors'
+import morgan from 'morgan'
 
-const server = express();
 
-const PORT = process.env.PORT || 8080;
-
+const server = express(); //crea servidor
+const PORT = process.env.PORT || 8080; //definir el puerto
 const ready = () => console.log("server ready: " + PORT);
+ 
+//middleware
+server.use(express.json()); //permite entradas y tambien trabajar con formato json
+server.use(express.urlencoded({ extended: true })); // permite capturar consultas 
+server.use(cors())  
+server.use(morgan('dev'))
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+//router
+server.use('/api' ,indexRouter)
 
 server.listen(PORT, ready); 
